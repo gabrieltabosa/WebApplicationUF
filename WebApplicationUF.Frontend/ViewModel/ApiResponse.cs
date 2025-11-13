d0e3af401e1cb3d5da888709f55b330d4afd0abe
Gabriel Ramos Correia Tabosa <gabriel.tabosa@detran.pe.gov.br>
2025-11-13 17:18:18 -0300

    refazendo o html

---
+++ WebApplicationUF.Frontend/ViewModel/ApiResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace WebUF.ViewModel
+{
+    public class ApiResponse<T>
+    {
+        public bool Sucesso { get; set; }       // indica se deu certo ou não
+        public T? Data { get; set; }            // dado retornado (pode ser qualquer tipo)
+        public string? Erro { get; set; }      // mensagem de erro, se houver
+
+        public ApiResponse() { }
+
+        public ApiResponse(T data)
+        {
+            Sucesso = true;
+            Data = data;
+        }
+
+        public ApiResponse(string error)
+        {
+            Sucesso = false;
+            Erro = error;
+        }
+    }
+}
